f60e8c5c7e213432ae3b474a8ab04d9b86e0f07d
Elisariane <elisarianeBS@gmail.com>
2025-02-08 22:51:50 -0300

    refactor: Remove try-catch and log

--- src/main/java/org/elisariane/processor/OrderLineParser.java
+++ src/main/java/org/elisariane/processor/OrderLineParser.java
@@ -1,10 +1,7 @@
 package org.elisariane.processor;
 
 import org.elisariane.dtos.OrderLine;
-import org.elisariane.exceptions.UnparseableLineException;
 import org.elisariane.exceptions.WrongLineSizeException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,23 +9,17 @@ import java.time.format.DateTimeFormatter;
 
 public class OrderLineParser {
 
-    private final Logger logger = LoggerFactory.getLogger(OrderLineParser.class);
     private static final int EXPECTED_LINE_LENGTH = 95;
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
 
     public OrderLine parse(String line) {
         validateLineSize(line);
 
-        try {
-            return extractOrderLine(line);
-        } catch (UnparseableLineException exception) {
-            throw new UnparseableLineException("Erro ao converter a linha: " + line, exception);
-        }
+        return extractOrderLine(line);
     }
 
     private void validateLineSize(String line) {
         if (line.length() != EXPECTED_LINE_LENGTH) {
-            logger.error("Linha com tamanho inválido ({} caracteres). Esperado: {}. Pulando linha...", line.length(), EXPECTED_LINE_LENGTH);
             throw new WrongLineSizeException("Tamanho de linha inválido.", EXPECTED_LINE_LENGTH, line.length());
         }
     }
